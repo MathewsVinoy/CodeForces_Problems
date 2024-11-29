e08db306be5074c4d68d0e7152f340884fcc82c0
mathews <mathewsvinoy@gmail.com>
2024-11-28 22:57:45 -0500

    nov 29

--- 900/Sakurako_and_Water.c
+++ 900/Sakurako_and_Water.c
@@ -1,12 +1,35 @@
 #include <stdio.h>
+
 int main(){
-    int array[2][2]={{-1,2},{3,0}},sum=0;
-    for(int i=0;i<2;i++){    
-        for(int j=0;j<2;j++){    
-            if(array[i][j]<0){
-                sum=sum+0-array[i][j];
-                printf("%d",0-array[i][j]);
-            }    
-        } 
-    }
+    int n,size,sum=0,temp=0,array[500][500];
+	scanf("%d",&size);
+	for(int k=0;k<size;k++){
+		sum=0;
+		scanf("%d",&n);
+		for(int i=0;i<n;i++)
+			for(int j=0;j<n;j++)
+				scanf("%d",&array[i][j]);
+
+		for(int i=0;i<n;i++){
+			if (array[i][i]<temp && array[i][i]<0)
+				temp=array[i][i];
+			}
+			sum+=(-temp);
+			temp=0;
+		for(int i=1;i<n;i++){
+			for(int j=0;j<n-i;j++)
+			if(array[j][j+i]<temp && array[j][j+i]<0)
+				temp=array[j][j+i];
+			sum+=(-temp);
+			temp=0;
+		}
+		for(int i=1;i<n;i++){
+			for(int j=0;j<n-i;j++)
+				if(array[j+i][j]<temp && array[j+i][j]<0)
+					temp=array[j+i][j];
+			sum+=(-temp);
+			temp=0;}
+		printf("%d\n",sum);
+	}
+	return 0;
 }
