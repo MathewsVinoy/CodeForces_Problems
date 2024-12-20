536442c5b77c0deca088d7de83449a059c09dd97
mathews <mathewsvinoy@gmail.com>
2024-12-20 11:21:03 -0500

    all most compleded

--- 1200/Bowling_Frame.c
+++ 1200/Bowling_Frame.c
@@ -1,6 +1,21 @@
 #include<stdio.h>
 int main(){
-	int size,limit=0;
-	printf("Hello World");
+	int t,w,b,sum=0;
+	scanf("%d",&t);
+	for(int j=0;j<t;j++){
+		int i=1,temp=0;
+		scanf("%d%d",&w,&b);
+		sum=w+b;
+		while(temp<sum){
+			temp=temp+i;
+			i++;
+		}
+		i--;
+		if(sum<temp){
+			temp=temp-i;
+			i--;
+		}
+		printf("%d\n",i);
+	}
 	return 0;
 }
