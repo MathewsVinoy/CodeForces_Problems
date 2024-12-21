74fd63fb248ff4d4aab9a44372b63990bcdae0b0
mathews <mathewsvinoy@gmail.com>
2024-12-21 11:31:44 -0500

    not completed

--- 1200/Uninteresting_Number.c
+++ 1200/Uninteresting_Number.c
@@ -1,25 +1,50 @@
 #include <stdio.h>
 #include <string.h>
-#include <stdlib.h>
-int main(){
-	int n,t;
-	char str[256];
-	scanf("%d",&t);
-	for(int i=0;i<t;i++){
-		int flag=1;
-		scanf("%s",&str);
-		for(int j=0;j<strlen(str);j++){
-			int num=str[i]-'0';
-			if(num<=3){
-				str[i]=num*num;
-				if((atoi(str)%9!=0))
-					flag=0;
-			}
-		}
-		if(flag==1)
-			printf("No\n");
-		else
-			printf("Yes\n");
-	}
-	return 0;
+
+int main() {
+    int t;
+    scanf("%d", &t);
+    for (int i = 0; i < t; i++) {
+        char str[100005];
+        scanf("%s", str);
+
+        int flag = 1;
+        int sum = 0;
+
+        for (int j = 0; str[j] != '\0'; j++) {
+            sum += str[j] - '0';
+        }
+
+
+        if (sum % 9 == 0) {
+            flag = 0;
+        } else {
+
+            for (int j = 0; str[j] != '\0'; j++) {
+                if (str[j] >= '0' && str[j] <= '3') {
+                    int num = str[j] - '0';
+                    int prod = num * num;
+                    char temp[100005];
+                    strcpy(temp, str);
+                    temp[j] = prod + '0';
+                    int new_sum = 0;
+                    for (int k = 0; temp[k] != '\0'; k++) {
+                        new_sum += temp[k] - '0';
+                    }
+
+                    if (new_sum % 9 == 0) {
+                        flag = 0;
+                        break;
+                    }
+                }
+            }
+        }
+
+        if (flag == 0) {
+            printf("YES\n");
+        } else {
+            printf("NO\n");
+        }
+    }
+    return 0;
 }
