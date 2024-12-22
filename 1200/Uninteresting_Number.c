35ba751d06d4635b22cc3d12bd18aee753a62c58
mathews <mathewsvinoy@gmail.com>
2024-12-22 22:34:56 +0530

    find the way to slove

--- 1200/Uninteresting_Number.c
+++ 1200/Uninteresting_Number.c
@@ -1,50 +1,59 @@
 #include <stdio.h>
 #include <string.h>
-
-int main() {
-    int t;
-    scanf("%d", &t);
-    for (int i = 0; i < t; i++) {
-        char str[100005];
-        scanf("%s", str);
-
-        int flag = 1;
-        int sum = 0;
-
-        for (int j = 0; str[j] != '\0'; j++) {
-            sum += str[j] - '0';
-        }
-
-
-        if (sum % 9 == 0) {
-            flag = 0;
-        } else {
-
-            for (int j = 0; str[j] != '\0'; j++) {
-                if (str[j] >= '0' && str[j] <= '3') {
-                    int num = str[j] - '0';
-                    int prod = num * num;
-                    char temp[100005];
-                    strcpy(temp, str);
-                    temp[j] = prod + '0';
-                    int new_sum = 0;
-                    for (int k = 0; temp[k] != '\0'; k++) {
-                        new_sum += temp[k] - '0';
-                    }
-
-                    if (new_sum % 9 == 0) {
-                        flag = 0;
-                        break;
-                    }
-                }
-            }
-        }
-
-        if (flag == 0) {
-            printf("YES\n");
-        } else {
-            printf("NO\n");
-        }
-    }
-    return 0;
+#include <stdlib.h>
+int main(){
+	int n,t;
+	char str[100],temp[100],c;
+	scanf("%d",&t);
+	for(int i=0;i<t;i++){
+		int flag=1;
+		scanf("%s",&str);
+		if((atoi(str)%9)==0)
+			flag=0;
+		strcpy(temp,str);
+		for(int j=0;j<strlen(str);j++){
+			strcpy(temp,str);
+			for(int k=0;k<strlen(str);k++){
+				if(j!=k){
+					int num1=temp[k]-'0';
+					if(num1<=3){
+						int prod=num1*num1;
+						temp[k] = prod+'0';
+						if((atoi(temp)%9)==0){
+							flag=0;
+							break;
+						}
+					}
+				}
+			}
+		}
+		for(int j=0;j<strlen(str);j++){
+			strcpy(temp,str);
+            int num1=temp[j]-'0';
+				if(num1<=3){
+					int prod=num1*num1;
+					temp[j] = prod+'0';
+					if((atoi(temp)%9)==0){
+						flag=0;
+						break;
+					}
+				}
+			for(int k=0;k<strlen(str);k++){
+				int num1=temp[k]-'0';
+				if(num1<=3){
+					int prod=num1*num1;
+					temp[k] = prod+'0';
+					if((atoi(temp)%9)==0){
+						flag=0;
+						break;
+					}
+				}
+			}
+		}
+		if(flag==0)
+			printf("YES\n");
+		else
+			printf("No\n");
+	}
+	return 0;
 }
