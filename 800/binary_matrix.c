009cc0595e63cc715ce9f7a06dd65f8e991fbe59
MathewsVinoy <mathewsvinoy@gmail.com>
2025-03-25 08:12:05 +0530

    inputting values

--- 800/binary_matrix.c
+++ 800/binary_matrix.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 
 int main(){
-	int i,n,m,t,arr[][];
+	int n,m,t,arr[100][100];
+	scanf("%d",&t);
+	while(t!=0){
+		scanf("%d%d",&n,&m);
+		for(int i =0; i<n;i++)
+			for(int j=0;j<m;j++)
+				scanf("%d",&arr[i][j]);
+		t--;
+	}
 	printf("hello world!");
 	return 0;
 }
