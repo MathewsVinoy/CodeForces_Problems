72893215e265b5c67e7d026935045fcd61cb2f8f
Mathews_Vinoy <mathewsvinoy@gmail.com>
2026-08-12 17:52:55 +0000

    new problem

---
+++ 800/threshold_movement.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int check(int t) {
+    if (t == 1)
+        return 0;
+    return 1;
+}
+
+int main(){
+    int n,t,w;
+    scanf("%d",&t);
+    int flag = check(t);
+    printf("Hello world");
+    return 0;
+}
+
