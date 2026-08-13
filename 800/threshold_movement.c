dcd61f0667adb6112b4fbc2355451713557bac1d
Mathews_Vinoy <mathewsvinoy@gmail.com>
2026-08-13 19:07:07 +0000

    trying with new logic:

--- 800/threshold_movement.c
+++ 800/threshold_movement.c
@@ -6,41 +6,6 @@ int main(){
         scanf("%d",&n);
         for(int i =0; i<n;i++)
             scanf("%d",&w[i]);
-        if(n==1){
-            printf("NO");
-            break;
-        }
-        int min =w[0], max=w[0];
-        for(int i=0;i<n;i++){
-            if(min>w[i])
-                min = w[i];
-            if(max<w[i])
-                max = w[i];
-        }
-        if(min==max){
-            printf("NO");
-        }
-        int out = min+max/2;
-        int flag = 0;
-        int l =0, r=0;
-        for(int i=0;i<n;i++){
-            if(w[i]==out){
-                flag =1;     
-                break;
-            }
-            else if(w[i]>out)
-                r++;
-            else
-                l++;
-        }
-        if(flag==1){
-            printf("NO");
-            break;
-        }
-        if(l==r)
-            printf("YES");
-        else
-            printf("NO");
     }
     return 0;
 }
