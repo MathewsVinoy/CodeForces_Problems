d29b1a499d2fde51658619e7dc61ae1be09a28e6
Mathews_Vinoy <mathewsvinoy@gmail.com>
2026-08-13 18:24:30 +0000

    new problem

--- 800/threshold_movement.c
+++ 800/threshold_movement.c
@@ -1,16 +1,47 @@
 #include <stdio.h>
 
-int check(int t) {
-    if (t == 1)
-        return 0;
-    return 1;
-}
-
 int main(){
-    int n,t,w;
-    scanf("%d",&t);
-    int flag = check(t);
-    printf("Hello world");
+    int n,t=1,w[100];
+    for(int j=0;j<t;j++){
+        scanf("%d",&n);
+        for(int i =0; i<n;i++)
+            scanf("%d",&w[i]);
+        if(n==1){
+            printf("NO");
+            break;
+        }
+        int min =w[0], max=w[0];
+        for(int i=0;i<n;i++){
+            if(min>w[i])
+                min = w[i];
+            if(max<w[i])
+                max = w[i];
+        }
+        if(min==max){
+            printf("NO");
+        }
+        int out = min+max/2;
+        int flag = 0;
+        int l =0, r=0;
+        for(int i=0;i<n;i++){
+            if(w[i]==out){
+                flag =1;     
+                break;
+            }
+            else if(w[i]>out)
+                r++;
+            else
+                l++;
+        }
+        if(flag==1){
+            printf("NO");
+            break;
+        }
+        if(l==r)
+            printf("YES");
+        else
+            printf("NO");
+    }
     return 0;
 }
 
