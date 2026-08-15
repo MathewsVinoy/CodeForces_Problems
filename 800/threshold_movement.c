3f5a46cd2e00bd5e52ba8bbfe9b867b21a9deff0
Mathews_Vinoy <mathewsvinoy@gmail.com>
2026-08-15 21:32:46 +0530

    completed

--- 800/threshold_movement.c
+++ 800/threshold_movement.c
@@ -1,12 +1,37 @@
 #include <stdio.h>
 
-int main(){
-    int n,t=1,w[100];
-    for(int j=0;j<t;j++){
-        scanf("%d",&n);
-        for(int i =0; i<n;i++)
-            scanf("%d",&w[i]);
+int main() {
+    int a,i;
+    scanf("%d", &a);
+    while(a--) {
+        int  n;
+        int w[100];
+        scanf("%d", &n);
+        for (i = 0; i < n; i++) {
+            scanf("%d", &w[i]);
+        }
+
+        if (n % 2 != 0) {
+            printf("NO\n");
+            continue;
+        }
+        int maxEven =w[1] , minodd =w[0];
+        for(i=0;i<n;i++){
+            if(i%2==0){
+                if(w[i]<minodd){
+                    minodd=w[i];
+                }
+            }else{
+                if(w[i]>maxEven){
+                    maxEven=w[i];
+                }
+            }
+        }
+        if(maxEven+1<minodd){
+            printf("YES\n");
+        }else{
+            printf("NO\n"); }
+    
     }
     return 0;
 }
-
