57cc0ea74c3ffad570b0612d40d1d5c391f48825
Mathews_Vinoy <mathewsvinoy@gmail.com>
2026-08-19 14:56:17 +0530

    19-8-2026

--- 800/Petya_and_Strings.c
+++ 800/Petya_and_Strings.c
@@ -5,17 +5,20 @@
 int main(){
     char s1[100],s2[100];
     scanf("%s%s",s1,s2);
-    int i,c1=0,c2=0;
-    for(i=0;i<strlen(s1);i++)
-        c1=c1+(int)tolower(s1[i]);
-    for(i=0;i<strlen(s2);i++)
-        c2=c2+(int)tolower(s2[i]);
-    printf("c1 -> %d, c2 -> %d",c1,c2);
-    if(c1<c2)
-        printf("-1");
-    else if(c2==c1)
+    int i,flag=0;
+    for(i=0;i<strlen(s1);i++){
+        if(tolower(s1[i])>tolower(s2[i])){
+            printf("1");
+            flag=1;
+            break;
+        }
+        if(tolower(s1[i])<tolower(s2[i])){
+            printf("-1");
+            flag=1;
+            break;
+        }
+    }
+    if(flag==0)
         printf("0");
-    else
-        printf("1");
     return 0;
 }
